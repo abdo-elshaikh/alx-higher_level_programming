7fc11cf695fe68e62ed5c23a8699d75c71d32a2d
abdo-mhmd <abdo202224@gmail.com>
2023-10-10 00:09:28 +0300

    update multiplay

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     """returns a tuple with the length of a string and its first character
     """
-    if len(sentence) <= 0:
+    if len(sentence) == 0:
         sentence[0] = None
     return (len(sentence), sentence[0])

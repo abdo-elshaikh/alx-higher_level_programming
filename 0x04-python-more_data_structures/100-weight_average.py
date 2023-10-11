14c0af27c8af1f4613a9ea8f74fc6689ac360dfc
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2023-10-11 17:50:42 +0300

    update tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """returns the weighted average of all integers tuple (<score>, <weight>)"""
+    """returns the weighted average of all 
+    
+    integers tuple (<score>, <weight>)"""
     if len(my_list) == 0:
         return 0
     total = 0
--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,44 +1,59 @@
-// Create two C functions that print some basic info about Python lists and Python bytes objects
-// Return 0 if successful, 1 otherwise.
-#include <python.h>
 #include "/usr/include/python3.4/Python.h"
 #include <stdio.h>
-void print_python_list(PyObject *p)
-{
-    PyObject *obj;
-    PyListObject *list_obj;
-    int list_size, i = 0;
-    /* print size of the list and allocations */
-    list_size = PyList_Size(p);
-    list_obj = (PyListObject *)p;
 
-    printf("[*] Size of the Python List = %d\n", list_size);
-    printf("[*] Allocated = %d\n", (int)(list_obj->allocated));
-    /* print elements of the list */
-    while (i < list_size)
-    {
-        obj = PyList_GetItem(p, i);
-        printf("Element %d: %s\n", i, Py_TYPE(obj)->tp_name);
-        i++;
-    }
+void print_hexn(const char *str, int n)
+{
+    int i = 0;
+
+    for (; i < n - 1; ++i)
+        printf("%02x ", (unsigned char)str[i]);
+
+    printf("%02x", str[i]);
 }
 
 void print_python_bytes(PyObject *p)
 {
-    PyObject *obj;
-    PyBytesObject *bytes_obj;
-    int list_size, i = 0;
-    /* print size of the list and allocations */
-    list_size = PyBytes_Size(p);
-    bytes_obj = (PyBytesObject *)p;
+    PyBytesObject *clone = (PyBytesObject *)p;
+    int calc_bytes, clone_size = 0;
 
-    printf("[*] Size of the Python Bytes = %d\n", list_size);
-    printf("[*] Allocated = %d\n", (int)(bytes_obj->allocated));
-    /* print elements of the list */
-    while (i < list_size)
+    printf("[.] bytes object info\n");
+    if (PyBytes_Check(clone))
     {
-        obj = PyBytes_GetItem(p, i);
-        printf("Element %d: %s\n", i, Py_TYPE(obj)->tp_name);
-        i++;
+        clone_size = PyBytes_Size(p);
+        calc_bytes = clone_size + 1;
+
+        if (calc_bytes >= 10)
+            calc_bytes = 10;
+
+        printf("  size: %d\n", clone_size);
+        printf("  trying string: %s\n", clone->ob_sval);
+        printf("  first %d bytes: ", calc_bytes);
+        print_hexn(clone->ob_sval, calc_bytes);
+        printf("\n");
+    }
+    else
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+    }
+}
+
+void print_python_list(PyObject *p)
+{
+    int i = 0, list_len = 0;
+    PyObject *item;
+    PyListObject *clone = (PyListObject *)p;
+
+    printf("[*] Python list info\n");
+    list_len = PyList_GET_SIZE(p);
+    printf("[*] Size of the Python List = %d\n", list_len);
+    printf("[*] Allocated = %d\n", (int)clone->allocated);
+
+    for (; i < list_len; ++i)
+    {
+        item = PyList_GET_ITEM(p, i);
+        printf("Element %d: %s\n", i, item->ob_type->tp_name);
+
+        if (PyBytes_Check(item))
+            print_python_bytes(item);
     }
 }

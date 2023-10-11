bfdf81c58309ac2dd6e3f526d522ff3805977b85
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2023-10-11 18:10:56 +0300

    update weight avrage

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """returns the weighted average of all 
-    
+    """returns the weighted average of all
     integers tuple (<score>, <weight>)"""
     if len(my_list) == 0:
         return 0

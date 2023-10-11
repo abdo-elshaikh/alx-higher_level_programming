ba92fe386f6c089cab0674b6e9bb5564f8ec0dcf
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2023-10-11 18:08:14 +0300

    update roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     """Convert a roman numeral to an integer."""
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     roman_dict = {
         'I': 1,
         'V': 5,
@@ -10,13 +12,12 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    int_value = 0
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
+    value = 0
     for i in range(len(roman_string)):
-        if roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            int_value += roman_dict[roman_string[i]] - \
+        if i > 0 and roman_dict[roman_string[i]] > \
+                roman_dict[roman_string[i - 1]]:
+            value += roman_dict[roman_string[i]] - \
                 2 * roman_dict[roman_string[i - 1]]
         else:
-            int_value += roman_dict[roman_string[i]]
-    return int_value
+            value += roman_dict[roman_string[i]]
+    return value

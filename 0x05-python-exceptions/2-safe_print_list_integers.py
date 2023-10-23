e9749bb5414636c5cc6a7c971e9bb745eeb6dd9c
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2023-10-23 20:13:38 +0300

    update printing list integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     """Print x and only integers elements of a list."""
     i = 0
+    len = 0
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
-        except ValueError:
-            pass
-        except TypeError:
+            len += 1
+        except (TypeError, ValueError):
             pass
         i += 1
     print()
-    return i
+    return len

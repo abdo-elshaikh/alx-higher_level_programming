091f93c5afb120c378ac14ca4be3a6d0ed0d970e
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2023-11-07 20:07:35 +0200

    update py input & output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ writes a string to a text file (UTF8) """
 
+
 def write_file(filename="", text=""):
     """ writes a string to a text file (UTF8)
         and returns the number of characters written

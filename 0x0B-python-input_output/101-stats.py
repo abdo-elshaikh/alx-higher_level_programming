84f3256acf23ee6f4467f6d52c3643be9592ad3e
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2023-11-07 21:22:53 +0200

    update stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
+"""
+Script that reads stdin line by line and computes metrics.
+"""
 
 import sys
-
+from collections import defaultdict
 
 if __name__ == "__main__":
-    status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
-    file_size = 0
-    number_of_lines = 0
+    status_counts = defaultdict(int)
+    total_file_size = 0
+    total_lines_read = 0
+    output_buffer = []
 
     for line in sys.stdin:
         line = line.strip()
         if not line:
             continue
+
         tokens = line.split()
-        status_codes[int(tokens[-2])] += 1
-        file_size += int(tokens[-1])
-        number_of_lines += 1
-        if number_of_lines == 10:
-            print("File size: {}".format(file_size))
-            for status_code, count in status_codes.items():
+        status_code = int(tokens[-2])
+        file_size = int(tokens[-1])
+
+        status_counts[status_code] += 1
+        total_file_size += file_size
+        total_lines_read += 1
+
+        if total_lines_read % 10 == 0:
+            output_buffer.append("File size: {}".format(total_file_size))
+            for status_code, count in sorted(status_counts.items()):
                 if count == 0:
                     continue
-                print("{}: {}".format(status_code, count))
-            number_of_lines = 0
+                output_buffer.append("{}: {}".format(status_code, count))
+
+            print("\n".join(output_buffer))
+            output_buffer = []
+
+    if output_buffer:
+        print("\n".join(output_buffer))

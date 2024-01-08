f2a29ee6972c89a8824264b4b3ebbf214c4bf8b3
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-01-08 20:55:06 +0200

    changed mode file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const args = process.argv;
-
-add(args[2], args[3]);
-
-function add(a, b) {
-    console.log(parseInt(a) + parseInt(b));
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
 }
+add(process.argv[2], process.argv[3]);

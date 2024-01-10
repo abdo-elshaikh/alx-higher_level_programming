26d526015fb38b7c8f5fe48703f67bbab776e39e
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-01-10 15:41:44 +0200

    update advanced tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,12 +4,3 @@ const newList = initList.map((number, index) => number * index);
 
 console.log(initList);
 console.log(newList);
-
-// Below are alternatives to newList function
-// const newList = initList.map(function (number, index) {
-//   return number * index;
-// });
-
-// const newList = initList.map((number, index) => {
-//   return number * index;
-// });
